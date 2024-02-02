0b0df1854fa2895db09eeee1ac7e1e1637bd4fe1
Bill <158589428+kiethoang112233@users.noreply.github.com>
2024-02-02 11:54:23 -0500

    Implement HIBP API call

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const { checkPasswordLeaked } = require('./passwordHelper');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/', async (req, res) => {
+    console.log(1);
+    const passwordToCheck = 'your_password_here';
+
+    checkPasswordLeaked(passwordToCheck)
+        .then((count) => {
+            if (count > 0) {
+                console.log(`The password has been leaked ${count} times. Consider choosing a different one.`);
+            } else if (count === 0) {
+                console.log('The password has not been leaked. It appears to be safe.');
+            } else {
+                console.log('Error checking the password.');
+            }
+        });
+
+    res.send(0);
 });
 
 app.listen(port, () => {
--- passwordHelper.js
+++ passwordHelper.js
@@ -1,4 +1,8 @@
-export function generateStrongPassword(length) {
+const axios = require('axios');
+const crypto = require('crypto');
+
+
+exports.generateStrongPassword = (length) => {
     const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
     const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const numericChars = '0123456789';
@@ -16,3 +20,32 @@ export function generateStrongPassword(length) {
 
     return password;
 }
+
+exports.checkPasswordLeaked = async (password) => {
+    const sha1Hash = await getSHA1Hash(password);
+    const prefix = sha1Hash.substring(0, 5);
+    const suffix = sha1Hash.substring(5);
+
+    try {
+        const response = await axios.get(`https://api.pwnedpasswords.com/range/${prefix}`);
+        const hashes = response.data.split('\n');
+
+        for (let i = 0; i < hashes.length; i++) {
+            const [hashSuffix, count] = hashes[i].split(':');
+            if (hashSuffix === suffix) {
+                return parseInt(count);
+            }
+        }
+
+        return 0;
+    } catch (error) {
+        console.error('Error checking password:', error);
+        return -1;
+    }
+};
+
+const getSHA1Hash = async (password) => {
+    const sha1sum = crypto.createHash('sha1');
+    sha1sum.update(password, 'utf-8');
+    return sha1sum.digest('hex').toUpperCase();
+};
